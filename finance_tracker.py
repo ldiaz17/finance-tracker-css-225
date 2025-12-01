d5036468f4df98b0cb7aa4a4a956e99db33015c3
ldiaz17 <ldiaz17@my.nl.edu>
2025-12-01 17:08:22 -0600

    finance_tracker.py
    This is my finance tracker for class. You can enter income and expenses, and it shows the total spent, remaining money, and a simple chart of the costs.

---
+++ finance_tracker.py
@@ -0,0 +1,36 @@
+# Finance Tracker CSS-225 
+# Author: Lea Diaz
+
+
+# Step 1: Income
+income = float(input("Enter your total income: "))
+
+# Step 2: List to store categories and amounts
+Categories = []
+Amount = []
+
+#Step 3: Input loop
+while True:
+  category = input("Enter expense category (or 'done' to finish): ")
+  if category.lower() == "done":
+    break 
+    amount = float(input(f" Enter amount for {category}: '))
+    categories.append(category)
+    amounts.append(amount)
+
+    # Step 4: Calculate totals
+    total_expenses = sum(amounts)
+    remaining_balance = income - total_expenses
+
+    #Step 5: Print summary
+    print("/n--- Finance Summary ---")
+    print("Total Income:", total_expenses)
+    print("Remaining Balance:", remaining_balance)
+    print("/nEcpenses by Category:")
+
+    #Step 6: Print each category with the amount
+    for i in range(len(categories)):
+        print(categories[i], ":", amounts[i])                
+
+    
+

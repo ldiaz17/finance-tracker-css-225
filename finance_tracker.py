9b51b98f4ee6515a66df72b8cfe79dafc806c9be
ldiaz17 <ldiaz17@my.nl.edu>
2025-12-01 18:03:12 -0600

    Update finance_tracker.py

--- finance_tracker.py
+++ finance_tracker.py
@@ -6,32 +6,32 @@
 income = float(input("Enter your total income: "))
 
 # Step 2: List to store categories and amounts
-Categories = []
-Amount = []
+categories = []
+amount = []
 
 #Step 3: Input loop
 while True:
   category = input("Enter expense category (or 'done' to finish): ")
   if category.lower() == "done":
     break 
-    amount = float(input(f" Enter amount for {category}: '))
+  amount = float(input(f"Enter amount for {category}: "))
     categories.append(category)
     amounts.append(amount)
 
-    # Step 4: Calculate totals
-    total_expenses = sum(amounts)
-    remaining_balance = income - total_expenses
+# Step 4: Calculate totals
+total_expenses = sum(amounts)
+remaining_balance = income - total_expenses
 
-    #Step 5: Print summary
-    print("/n--- Finance Summary ---")
-    print("Total Income:", total_expenses)
-    print("Remaining Balance:", remaining_balance)
-    print("/nEcpenses by Category:")
+#Step 5: Print summary 
+print("\n--- Finance Summary ---")
+print("Total Income:", income)
+print("Remaining Balance:", remaining_balance)
+print("\nExpenses by Category:")
 
-    #Step 6: Print each category with the amount
-    for i in range(len(categories)):
-         bar = '*' * int(amount[i] // 10)                 
-        print(categories[i], ":", amounts[i])                
+#Step 6: Print each category with the amount
+for i in range(len(categories)):
+  bar = '*' * int(amount[i] // 10)
+print(categories[i], ":", amounts[i], "|", bar)                
 
     
 
